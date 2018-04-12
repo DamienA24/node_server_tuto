daf97b8b33cc87663b8f95f67b1d1c865ac155b0
Damien Alves <alves.d24@laposte.net>
2018-04-12 11:14:36 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  let now = new Date().toString();
+  let log = `${now} ${req.method} ${req.url}`;
+
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  })
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs');
+})
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+})
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle: 'Page first',
+    welcomeMessage: `hey guy's !`
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'about page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    erroMessage: 'error'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('server listening on port 3000');
+})
